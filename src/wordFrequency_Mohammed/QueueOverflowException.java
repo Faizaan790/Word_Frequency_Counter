440ce1b4a5f03ba927604a5fe54ffe6e771b3d05
Mohammed Faizaan Khan <faizaanhaque329@gmail.com>
2023-06-14 21:11:42 -0500

    Initial commit

---
+++ src/wordFrequency_Mohammed/QueueOverflowException.java
@@ -0,0 +1,14 @@
+package wordFrequency_Mohammed;
+
+public class QueueOverflowException extends RuntimeException
+{
+  public QueueOverflowException()
+  {
+    super();
+  }
+
+  public QueueOverflowException(String message)
+  {
+    super(message);
+  }
+}
---
+++ src/wordFrequency_Mohammed/QueueUnderflowException.java
@@ -0,0 +1,14 @@
+package wordFrequency_Mohammed;
+
+public class QueueUnderflowException extends RuntimeException
+{
+  public QueueUnderflowException()
+  {
+    super();
+  }
+
+  public QueueUnderflowException(String message)
+  {
+    super(message);
+  }
+}
